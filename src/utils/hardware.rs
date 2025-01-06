8f1dbae95f1035e996110c71618f09a37e1e7203
NoOPeEKS <arnauapps@gmail.com>
2025-01-06 18:52:30 +0100

    feat: get shell name for hardware info

--- src/utils/hardware.rs
+++ src/utils/hardware.rs
@@ -8,6 +8,10 @@ fn get_tty() -> Option<String> {
     env::var("TERM").ok()
 }
 
+fn get_shell() -> Option<String> {
+    env::var("SHELL").ok()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,5 +20,11 @@ mod tests {
     fn get_tty_name() {
         env::set_var("TERM", "tmux-256color");
         assert_eq!(get_tty(), Some("tmux-256color".to_string()));
+    }
+
+    #[test]
+    fn get_shell_name() {
+        env::set_var("SHELL", "/bin/zsh");
+        assert_eq!(get_shell(), Some("/bin/zsh".to_string()));
     }
 }
